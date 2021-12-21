5fe354d205bc55d0629f290f94cff559d3dbabfa
jairosl <jairo.lima@aluno.uepb.edu.br>
2021-12-20 22:36:54 -0300

    chore: config styled-component and babel plugin

---
+++ src/pages/_document.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+class MyDocument extends Document {
+  static async getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage(
+      (App) => (props) => sheet.collectStyles(<App {...props} />)
+    );
+
+    const styleTags = sheet.getStyleElement();
+
+    return { ...page, styleTags };
+  }
+
+  render() {
+    return (
+      <Html lang="pt-br">
+        <Head>
+          {this.props.styleTags}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from 'styled-components'
+
+const H1 = styled.h1`
+  color: red;
+
+`;
+
+
+export default function Home() {
+  return (
+    <>
+      <H1>Next.js + Styled Component</H1>
+    </>
+  )
+}
