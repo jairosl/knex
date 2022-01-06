94d46bdb6c96323b5c78a213bc193058538ebca1
luketeeeeee <henrique.lucas521@gmail.com>
2022-01-06 18:04:05 -0300

    created services component and imported to the main component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Main from '../components/Home';
+import Services from '../components/Services';
 
 export default function Home() {
     return (
         <>
             <Main />
+            <Services />
         </>
     );
 }
