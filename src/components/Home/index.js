1ab24214f4e263f39408c68741fcb819edcef837
Callyel Alves <callyelalves@hotmail.com>
2022-01-07 20:04:41 -0300

    create home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,37 @@
+import Image from 'next/image';
 import React from 'react';
+import { Wrapper, Content, Title, Container, Button } from './styles.js';
 
 export default function Home() {
-    return <h1>Home</h1>;
+    return (
+        <Wrapper>
+            <Content>
+                <Title>Atualize seu negócio</Title>
+                <p>
+                    Temos como principal objetivo entregar o melhor site para o seu negócio
+                    alavancar de vendas e ainda com baixo preço!
+                </p>
+
+                <Button>Conheça nossos serviços</Button>
+                <Button color>Faça Seu Orçamento</Button>
+                <span>Acompanhe-nos nas redes sociais!</span>
+                <Image src="/assets/images/iconInstagram.svg" width={34} height={34} />
+                <span>@Knex</span>
+                <Image src="/assets/images/iconLinkedin.svg" width={34} height={34} />
+                <span>linkedin/knex</span>
+                <Image src="/assets/images/iconFacebook.svg" width={34} height={34} />
+                <span>facebook.com/knex</span>
+            </Content>
+            <Container>
+                <div>
+                    <Image
+                        src="/assets/images/femaleWorking.svg"
+                        alt="avatar"
+                        width={863}
+                        height={644}
+                    />
+                </div>
+            </Container>
+        </Wrapper>
+    );
 }
--- src/components/Home/styles.js
+++ src/components/Home/styles.js
@@ -1,3 +1,48 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Wrapper = styled.div`
+    padding: 0 10rem;
+    height: 100vh;
+    display: flex;
+    background: var(--black);
+    gap: 4rem;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const Content = styled.div`
+    width: 36rem;
+    p,
+    span {
+        font-size: 1.125rem;
+        line-height: 30px;
+        color: var(--white);
+    }
+`;
+
+export const Container = styled.div`
+    width: 800px;
+    height: 644px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const Title = styled.h2`
+    color: #8bffa2;
+    margin: 0;
+    font-weight: 600;
+    font-size: 3.5rem;
+    line-height: 72px;
+`;
+
+export const Button = styled.button`
+    background-color: ${(props) => (props.color ? 'var(--black)' : '#8bffa2')};
+    padding: 0.8rem;
+    border-radius: 1.6rem;
+    border: none;
+    margin-top: 2rem;
+    margin-right: 0.2rem;
+    font-size: 1rem;
+    font-weight: 600;
+`;
