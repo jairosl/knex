6c33447df9ea3c37d236aa495ac4e27969c8ef45
natan <natancavalcant@gmail.com>
2022-01-11 13:05:34 -0300

    Adicionar e estilizar componente de chamativo para contato

--- src/components/ContactUs/index.js
+++ src/components/ContactUs/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import Image from 'next/image';
+
+import { ContactUsContainer, LikeText, ContactUsContent } from './styles.js';
 
 export default function ContactUs() {
-    return <h1>Contact Us</h1>;
+    return (
+        <ContactUsContainer>
+            <ContactUsContent>
+                <Image src="/assets/images/call.webp" alt="Call" width={963} height={518} />
+                <LikeText>
+                    <h1>Gostou da Knex? Então entre em contato conosco e faça seu site!</h1>
+                </LikeText>
+            </ContactUsContent>
+        </ContactUsContainer>
+    );
 }
