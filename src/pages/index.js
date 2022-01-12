ce708b301d6267b0afd81bdb78de26f92b109fdf
natan <natancavalcant@gmail.com>
2022-01-12 20:56:50 -0300

    trocar a ordem dos componentes e ajustar o padding de vantagens e argumentos

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,8 +11,8 @@ export default function Home() {
     return (
         <>
             <Main />
-            <Services />
             <AdvantagesAndArguments />
+            <Services />
             <AboutUS />
             <ContactUs />
             <Footer />
