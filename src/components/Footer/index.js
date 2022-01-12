bdb12998d47b96ed17c46e04cbe4bba1214adf4b
natan <natancavalcant@gmail.com>
2022-01-12 19:29:04 -0300

    Estilizar componente de footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,58 +3,89 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
+import {
+    FooterContainer,
+    FooterContent,
+    KnexInformation,
+    DomainColumn,
+    NavLinks,
+    LegalInformation
+} from './styles';
+
 export default function Footer() {
     return (
-        <footer>
-            <div>
-                <div>
-                    <Image src="/assets/images/knex-logo.svg" alt="knex" width={136} height={69} />
-                    <h1>Soluções web para a sua empresa</h1>
+        <FooterContainer>
+            <FooterContent>
+                <KnexInformation>
+                    <DomainColumn>
+                        <Image
+                            src="/assets/images/knex-logo.svg"
+                            alt="knex"
+                            width={136}
+                            height={69}
+                        />
+                        <h1>
+                            Soluções web para
+                            <br /> a sua empresa
+                        </h1>
 
-                    <span>
-                        <Image
-                            src="/assets/images/iconFacebook.svg"
-                            alt="facebook"
-                            width={17}
-                            height={17}
-                        />
-                        <Image
-                            src="/assets/images/iconInstagram.svg"
-                            alt="facebook"
-                            width={17}
-                            height={17}
-                        />
-                    </span>
+                        <span>
+                            <span>
+                                <Image
+                                    src="/assets/images/whiteFacebookIcon.svg"
+                                    alt="facebook"
+                                    width={17}
+                                    height={17}
+                                />
+                            </span>
+                            <span>
+                                <Image
+                                    src="/assets/images/whiteInstagramIcon.svg"
+                                    alt="facebook"
+                                    width={17}
+                                    height={17}
+                                />
+                            </span>
+                        </span>
+                    </DomainColumn>
+
+                    <NavLinks>
+                        <h1>Empresa</h1>
+                        <Link href="/#about">
+                            <a>Sobre nós</a>
+                        </Link>
+                        <Link href="/#mej">
+                            <a>Sobre o MEJ</a>
+                        </Link>
+                        <Link href="/#services">
+                            <a>Serviços</a>
+                        </Link>
+                        <Link href="/#members">
+                            <a>Membros</a>
+                        </Link>
+                    </NavLinks>
+                    <NavLinks>
+                        <h1>Contato</h1>
+                        <Link href="mailto:knex.org@gmail.com">
+                            <a>knex.org@gmail.com</a>
+                        </Link>
+                        <Link href="tel:+5583912345678">
+                            <a>(83) 9 1234-5678</a>
+                        </Link>
+                    </NavLinks>
+                </KnexInformation>
+                <LegalInformation>
                     <p>&copy; All rights reserved - Knex</p>
-                </div>
-
-                <div>
-                    <h1>Empresa</h1>
-                    <Link href="/">
-                        <a>Sobre nós</a>
-                    </Link>
-                    <Link href="/">
-                        <a>Sobre o MEJ</a>
-                    </Link>
-                    <Link href="/">
-                        <a>Serviços</a>
-                    </Link>
-                    <Link href="/#">
-                        <a>Membros</a>
-                    </Link>
-                </div>
-                <div>
-                    <h1>Contato</h1>
-                    <Link href="mailto:knex.org@gmail.com">
-                        <a>knex.org@gmail.com</a>
-                    </Link>
-                    <Link href="tel:+5583912345678">
-                        <a>(83) 9 1234-5678</a>
-                    </Link>
-                </div>
-                <p>Política de Privacidade</p>
-                <p>Termos e Condições</p>
-            </div>
-        </footer>
+                    <span>
+                        <Link href="/">
+                            <a>Política de Privacidade</a>
+                        </Link>
+                        <Link href="/">
+                            <a>Termos e Condições</a>
+                        </Link>
+                    </span>
+                </LegalInformation>
+            </FooterContent>
+        </FooterContainer>
     );
 }
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,3 +1,87 @@
 import styled from 'styled-components';
 
-export const FooterContainer = styled.section``;
+export const FooterContainer = styled.section`
+    background: #0d0c17;
+`;
+
+export const FooterContent = styled.div`
+    max-width: 1300px;
+    padding: 5rem 2rem;
+
+    margin: 0 auto;
+
+    display: flex;
+    flex-direction: column;
+
+    color: var(--white);
+
+    p {
+        font-size: 16;
+        font-weight: 500;
+    }
+
+    h1 {
+        font-size: 1.5rem;
+        font-weight: 600;
+    }
+
+    .alignEnd {
+        align-self: flex-end;
+    }
+`;
+
+export const KnexInformation = styled.div`
+    max-width: 830px;
+    display: grid;
+
+    grid-template-columns: 50% 25% 25%;
+
+    @media (max-width: 720px) {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+    }
+`;
+
+export const LegalInformation = styled.div`
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    gap: 2rem;
+
+    @media (max-width: 720px) {
+        flex-direction: column;
+        align-items: center;
+    }
+
+    span {
+        display: flex;
+        align-items: center;
+        gap: 3rem;
+
+        @media (max-width: 720px) {
+            flex-direction: column;
+            gap: 1rem;
+        }
+    }
+
+    margin-top: 5rem;
+`;
+
+export const DomainColumn = styled.div`
+    span {
+        span + span {
+            margin-left: 1rem;
+        }
+    }
+`;
+
+export const NavLinks = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 0.75rem;
+
+    h1 {
+        font-size: 1.125rem;
+    }
+`;
