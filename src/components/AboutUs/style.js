b30c673ac877f91b241846d4aaa777eac8308116
João Damião <thejoaodamiao@gmail.com>
2022-01-13 12:24:01 -0300

    Criar responsividade para AboutUs

--- src/components/AboutUs/style.js
+++ src/components/AboutUs/style.js
@@ -7,34 +7,82 @@ export const Container = styled.div`
     align-items: center;
     font-weight: 600;
     font-size: 0.9em;
+    
+    
+    @media screen and (max-width: 600px) {
+          padding-bottom: 15rem;
+    }
+    @media screen and (max-width: 400px) {
+          padding-bottom: 20rem;
+    }
+  
+   
+
 `;
 
+export const LeftCol = styled.div`
+  width: 60%;
+  margin: 0 auto;
+  @media screen and (max-width: 600px) {
+          height: 100%;
+  }
+
+`
+
 export const MainText = styled.div`
-    width: 700px;
+    width: 80%;
+    margin-right: 1rem;
+
     h1 {
         font-size: 48px;
         margin: 0;
+
+        @media screen and (max-width: 600px) {
+          text-align: center;
+        }
+
+        @media screen and (max-width: 910px) {
+          font-size: 2.5rem;
+          line-height: 56px;
+        }
     }
     p {
-        color: #5b5b5b;
-        width: 400px;
+        
+        color: var(--gray);
+        width:100%;
+        
+    }
+
+    @media screen and (max-width: 800px) {
+            margin: 0;
+            text-align: center;
+    }
+
+    @media screen and (max-width: 910px) {
+        width: 100%;
+        margin-right: 1rem;
+
     }
 `;
 export const SubText = styled.div`
-    width: 800px;
+    width: 100%;
     display: flex;
-
-    div:first-child {
-        margin-right: 1rem;
-    }
 
     h3{
         margin-block: 0;
     }
 
     p {
-        color: #5b5b5b;
-        width: 340px;
+        color: var(--gray);
+        width: 85%;
+        @media screen and (max-width: 800px) {
+          text-align: center;
+        }
+
+        @media screen and (max-width: 910px) {
+        width: 85%;
+    }
+       
     }
 
     div {
@@ -42,5 +90,35 @@ export const SubText = styled.div`
         flex-direction: column;
         justify-content: flex-start;
         align-items: flex-start;
+        width: 40%;
+
+        @media screen and (max-width: 800px) {
+          margin: 0 auto;
+          align-items: center;
+          width: 80%;
+       
+      }
+
+
+    }
+
+    @media screen and (max-width: 800px) {
+      margin: 0 auto;
+      display: block;
+
+       
+    }
+
+    @media screen and (max-width: 910px) {
+        width: 100%;
+        margin-right: 1rem ;
     }
 `;
+
+export const RightCol = styled.div`
+  width: 33%;
+  @media screen and (max-width: 800px) {
+      display: none ;
+  }
+
+`
