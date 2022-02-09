7af6d0c2a5383503eb945063df77ebce09486c15
jairosl <jairo.lima@aluno.uepb.edu.br>
2022-02-09 19:12:51 -0300

    feat: retrun 400 without correct params

--- src/pages/api/send.js
+++ src/pages/api/send.js
@@ -1,29 +1,51 @@
-var SibApiV3Sdk = require('sib-api-v3-sdk');
-var defaultClient = SibApiV3Sdk.ApiClient.instance;
+import { ApiRequest } from '../../utils/api/validation';
+
+const SibApiV3Sdk = require('sib-api-v3-sdk');
+const defaultClient = SibApiV3Sdk.ApiClient.instance;
 
 // Configure API key authorization: api-key
 var apiKey = defaultClient.authentications['api-key'];
 apiKey.apiKey = process.env.API_KEY;
 
 export default async (request, response) => {
-    var apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
-
-    var sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail(); // SendSmtpEmail | Values to send a transactional email
-
-    sendSmtpEmail.subject = 'My {{params.subject}}';
-    sendSmtpEmail.htmlContent =
-        '<html><body><h1>This is my first transactional email {{params.parameter}}</h1></body></html>';
-    sendSmtpEmail.sender = { name: 'John Doe', email: 'example@example.com' };
-    sendSmtpEmail.to = [{ email: 'jairosoareslima@gmail.com', name: 'Jane Doe' }];
-    sendSmtpEmail.cc = [{ email: 'example2@example2.com', name: 'Janice Doe' }];
-    // sendSmtpEmail.bcc = [{ email: 'John Doe', name: 'jairosoareslima@gamil.com' }];
-    sendSmtpEmail.params = { parameter: 'jairo', subject: 'teste' };
+    const { name, subject, email, contact } = request.body;
 
     try {
-        const data = await apiInstance.sendTransacEmail(sendSmtpEmail);
-        response.json({ 'API called successfully. Returned data: ': data });
+        ApiRequest.validateSync(request.body, { abortEarly: false });
+        const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+
+        const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail(); // SendSmtpEmail | Values to send a transactional email
+
+        sendSmtpEmail.subject = 'Possível Cliente: {{params.name}}';
+        sendSmtpEmail.htmlContent = `
+          <html>
+              <body>
+                  <p>Email: {{params.email}}</p>
+
+                  <p>Assunto: {{params.subject}}</p>
+
+                  <p>Contato: {{params.contact}}</p>
+
+              </body>
+          </html>`;
+        sendSmtpEmail.sender = { name: 'site Knex', email: 'knex.org@gmail.com' };
+        sendSmtpEmail.to = [{ email: 'knex.org@gmail.com', name: 'Knex' }];
+        sendSmtpEmail.cc = [{ email: 'jairosoareslima@gamil.com', name: 'Jairo' }];
+        sendSmtpEmail.params = {
+            name: name,
+            subject: subject,
+            email: email,
+            contact: contact
+        };
+
+        try {
+            const data = await apiInstance.sendTransacEmail(sendSmtpEmail);
+            response.json({ 'API called successfully. Returned data: ': data });
+        } catch (e) {
+            console.error(e);
+            response.status(400);
+        }
     } catch (e) {
-        console.error(e);
-        response.status(400);
+        response.status(400).json({ error: e.errors });
     }
 };
